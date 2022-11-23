d606bb9af490d73432b4dec2e2bb93d8d08f3e0d
Yera <yera9410@gmail.com>
2022-11-23 08:22:54 +0000

    feat : add faster-rcnn_swin with focal loss
    - Focal Loss로 classification 학습한 faster-rcnn
    - backbone을 swin으로 변경한 faster-rcnn
    - optimizer - AdamW로 바꾼 step scheduler
    - Focal Loss의 출처 retinanet config

---
+++ baseline/mmdetection/configs/_2stage_/faster_rcnn_swin_fcloss.py
@@ -0,0 +1,55 @@
+_base_ = [
+    './_base_/datasets/coco_detection.py', 
+    './_base_/models/faster_rcnn_r50_fpn_foloss.py', 
+    './_base_/schedules/schedule_1x_adamw.py', 
+    './_base_/default_runtime.py'
+]
+
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
+model = dict(
+    backbone=dict(
+        _delete_=True,      # 덮어쓰기
+        type='SwinTransformer',
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+    neck=dict(in_channels=[96, 192, 384, 768])
+)
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='MMDetWandbHook',
+            #  wandb init 설정
+            init_kwargs=dict(
+                entity = 'miho',
+                project = 'Detection-Competition', 
+                name = '1123_yr_faster-rcnn-focalloss', 
+                tags = ['swin', 'adamw', 'faster_rcnn', 'focal'],
+                notes = 'faster-rcnn, swin으로 학습 및 wandb 설정 테스트해본 것', 
+            ),
+             
+            # Logging interval (iterations)
+            interval = 10,
+            log_checkpoint=True,
+            log_checkpoint_metadata=True,
+            # The number of validation images to be logged. If zero, the evaluation won't be logged. Defaults to 100.
+            num_eval_images=100,
+            # Threshold for bounding box scores. Defaults to 0.3.
+            bbox_score_thr=0.3
+        )
+    ]
+)
