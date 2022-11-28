86a200f89e5b27cc0686415110dd337da3152aa0
KH <ganghe74@gmail.com>
2022-11-28 09:22:18 +0000

    feat: add tta configs
    TTA가 성능에 미치는 영향을 테스트하기 위한 Config

---
+++ baseline/UniverseNet/configs/_base_/datasets/trash_mstrain_512_1024_tta1.py
@@ -0,0 +1,57 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/opt/ml/dataset/'
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 512), (1333, 1024)],
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=[(1333, 800), (1333, 800), (1333, 1024), (1333, 1024)],
+        flip=[False, True, False, True],
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        classes = classes, 
+        ann_file=data_root + 'train-kfold-0.json',
+        img_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        classes = classes, 
+        ann_file=data_root + 'val-kfold-0.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes = classes, 
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
