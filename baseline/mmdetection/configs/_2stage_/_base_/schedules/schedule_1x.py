14ec5d433de62e0829ddd3328d549c83ef70befd
Yera <yera9410@gmail.com>
2022-11-28 08:03:04 +0000

    fix : change max_epochs

--- baseline/mmdetection/configs/_2stage_/_base_/schedules/schedule_1x.py
+++ baseline/mmdetection/configs/_2stage_/_base_/schedules/schedule_1x.py
@@ -11,4 +11,4 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=12)
+runner = dict(type='EpochBasedRunner', max_epochs=20)
